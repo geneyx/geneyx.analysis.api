b4b4eb687c76a2bd161623935c5a2409bf53b148
Yaara Unger <yaara@geneyx.com>
2024-11-26 16:30:40 +0200

    Change ONT unifier to receive an additional flag, stating whether the repeats SVTYPE should be changed (for the current caller, STRaglr) or not (for the future caller, MahiMahi)

--- scripts/UnifyVcf/ONTUnifyVcf.py
+++ scripts/UnifyVcf/ONTUnifyVcf.py
@@ -9,9 +9,14 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--svPath', help='SV input file path (optional)', required=False, default=None)
     parser.add_argument('-c', '--cnvPath', help='CNV input file path (optional)', required=False, default=None)
     parser.add_argument('-r', '--repeatPath', help='repeats input file path (optional)', required=False, default=None)
-
+    parser.add_argument('-modify', action='store_true', help='a flag indicating whether to modify the repeat calls or not. Add this flag when STRaglr is the repeat caller')
 
     args = parser.parse_args()
     # a flag indicating whether to add the SVTYPE=REP to the repeats lines or not
-    skip_svtype = True
+    if(args.modify):
+        skip_svtype = False
+    else:
+        skip_svtype = True
+
     run(args.outputPath, args.svPath, args.cnvPath, args.repeatPath, skip_svtype)
+
